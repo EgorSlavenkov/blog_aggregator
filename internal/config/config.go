1c5b1c15839aad76878fd71df5a46452c8c8aa1a
EgorSlavenkov <bakketpw@mail.ru>
2025-01-22 10:04:32 +0500

    setup local config with new funcs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,68 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
-// func Read()Config{
+func Read() (Config, error) {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
 
-// }
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+	return write(*c)
+}
+
+func getConfigFilePath() (string, error) {
+	path, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	path = path + "/" + configFileName
+	return path, nil
+}
+
+func write(cfg Config) error {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/EgorSlavenkov/blog_aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	cfg.SetUser("egor")
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("DB URL: %s, Current User: %s\n", cfg.DbURL, cfg.CurrentUserName)
 }
